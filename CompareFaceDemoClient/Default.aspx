0528357115824c10ddf492aba7cf8b18aa6cce52
WlmShk <zerobonus@gmail.com>
2020-04-08 14:16:22 +0300

    Demo Client

---
+++ CompareFaceDemoClient/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label3" runat="server" Text="Face Compare - "></asp:Label>
+        <br />
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+        </div>
+        <asp:CheckBox ID="chkCompareBase" runat="server" Text="Este prima imagine (baza cu care comparam)?" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="Download" OnClick="Button2_Click" Visible="False" />
+
+        <hr />
+        <asp:Label ID="Label1" runat="server" Text="Compara imaginea cu imaginea de baza"></asp:Label>
+        <hr />
+        <asp:Image ID="Image1" runat="server" Height="100" Width="100" />
+        <hr />
+        <asp:Label ID="Label2" runat="server" Text="Imagine de baza"></asp:Label>
+        <hr />
+        <asp:Image ID="Image2" runat="server" Height="100" Width="100" />
+    </form>
+
+</body>
+</html>
---
+++ CompareFaceDemoClient/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    //Upload
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            Label3.Text = "Face Compare -";
+            Proxy.ServiceClient client = new Proxy.ServiceClient();
+            string folderPath = Server.MapPath("~/Files/");
+
+            //Check whether Directory (Folder) exists.
+            if (!Directory.Exists(folderPath))
+            {
+                //If Directory (Folder) does not exists Create it.
+                Directory.CreateDirectory(folderPath);
+            }
+
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+
+
+            FileUpload1.PostedFile.InputStream.Seek(0, SeekOrigin.Begin);
+            using (var binaryReader = new BinaryReader(FileUpload1.PostedFile.InputStream))
+            {
+                byte[] fileBytes = binaryReader.ReadBytes(FileUpload1.PostedFile.ContentLength);
+                bool compareBase = chkCompareBase.Checked;
+                var fileName = client.UploadBytes(fileBytes, compareBase);
+                if (compareBase)
+                {
+                    Label3.Text = "Face Compare - Imaginea de baza a fost incarcata pe server.";
+                    Image2.ImageUrl = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+                }                                
+                else
+                {
+                    Label3.Text = "Face Compare - " + fileName;
+                    Image1.ImageUrl = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+                }
+                Session["file"] = fileName;
+                chkCompareBase.Checked = false;
+            }
+        }
+        //var fileName = client.Upload(FileUpload1.FileContent);
+        //Session["file"] = fileName;
+    }
+    //Download
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Proxy.ServiceClient client = new Proxy.ServiceClient();
+        var stream = client.Download(Session["file"].ToString());
+        StreamReader reader = new StreamReader(stream);
+        Response.Write(reader.ReadToEnd());
+    }
+}
